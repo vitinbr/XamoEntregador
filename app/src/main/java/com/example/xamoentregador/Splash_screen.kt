a331948aea786e4dca784780fad9eaa379d4be37
vitinbr <vitorhsouza13@gmail.com>
2020-05-21 15:30:02 -0400

    jk

---
+++ app/src/main/java/com/example/xamoentregador/Splash_screen.kt
@@ -0,0 +1,13 @@
+package com.example.xamoentregador
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash_screen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.entrega_andamento_3)
+
+    }
+}
